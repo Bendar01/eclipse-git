32146db0efbfe649ab349106f9d1ecdb123f82d3
Saha <bendar01@tut.by>
2018-07-20 13:04:44 +0300

    I have just created a new java project

---
+++ Git-repasitory/src/Saha/git/by/color.java
@@ -0,0 +1,19 @@
+package Saha.git.by;
+
+import java.awt.Choice;
+
+public class color {
+
+	public static void main (String args[]){
+		Choice color = new Choice();
+		color.add("Белый");
+		color.add("Зеленый");
+		color.add("Синий");
+		color.add("Черный");
+		add(color);
+	}
+		private static void add(Choice color) {
+			
+			
+		}
+	}
